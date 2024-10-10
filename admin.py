d1886442befad626a4008c55065bcc2e697e5762
Huynhgiang123 <129807117+Huynhgiang123@users.noreply.github.com>
2024-10-10 12:55:56 +0700

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,7 @@
+
+from django.contrib import admin
+class CommentAdminSite(admin.AdminSite):
+ title_header = 'c8admin'
+ site_header = 'c8admin'
+ index_title = 'Welcome to c8admin'
+ logout_template = "comment8or/logged_out.html"
